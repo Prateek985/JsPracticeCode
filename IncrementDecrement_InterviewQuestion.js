b9fc6bc62fbda5dbe0c2735a452406b415ca2b23
Prateek985 <prateekchouhan171@yahoo.com>
2026-02-17 20:55:49 +0530

    This is IncrementDecrement_InterviewQuestions.

---
+++ IncrementDecrement_InterviewQuestion.js
@@ -0,0 +1,33 @@
+//let i = 11;
+//let j = i++ + ++i;
+//console.log(i);//13
+//console.log(j);//24
+
+//let a = 11, b = 22;
+//let c = a + b + a++ + b++ + ++a + ++b;
+//console.log("a = " + a);//13
+//console.log("b = " + b);//24
+//console.log("c = " + c);//103
+
+//let i=0;
+//let j = i++ - --i + ++i - i--;
+//console.log(i);//0
+//console.log(j);//0
+
+//let i=19, j=29, k=9;
+//let m = i-- - j-- - k--;
+//console.log("i = " + i);//18
+//console.log("j = " + j);//28
+//console.log("k = " + k);//8
+//console.log("m = " + m);//-19
+
+//let m=0, n=0;
+//let p = --m * --n * n-- * m--;
+//console.log(p);//1
+//console.log(m);//-2
+//console.log(n);//-2
+
+//let d = 1.5, D = 2.5;
+//console.log(d++ + ++D);//5.0
+//console.log(d);//2.5
+//console.log(D);//3.5
